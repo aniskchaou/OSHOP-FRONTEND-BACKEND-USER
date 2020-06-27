455210a710ce6f05d1a999bff3f19e5573a58a0f
anis kchaou <aniskchaou@gmail.com>
2020-06-27 15:16:21 +0200

    bug fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/organicShop/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
